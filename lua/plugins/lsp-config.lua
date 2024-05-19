925c8e5ae4d5eb493d98482fc025a2f9d0668a58
Daniel Chico <chico@generalsoftwareinc.com>
2024-05-19 16:21:58 -0400

    refactor: Improve LSP configuration and keymap setup

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,29 +1,30 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = true
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" , "pyright"}
-            }
-            )
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.pyright.setup({})
-            vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, {})
-            vim.keymap.set('n', 'gh', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
+	{
+		"williamboman/mason.nvim",
+		config = true,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.pyright.setup({})
+			vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, {})
+			vim.keymap.set("n", "gh", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
             vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, {})
         end
     }
+		end,
+	},
 }
-
