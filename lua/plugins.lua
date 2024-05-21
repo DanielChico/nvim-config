d2f33048b303090bd5cecba9e53a72986c20ea63
Daniel Chico <chico@generalsoftwareinc.com>
2024-05-20 21:54:12 -0400

    feat: add neodev

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,5 +1,6 @@
 return {
 	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-    { 'numToStr/Comment.nvim', opts = {} },
+	{ "numToStr/Comment.nvim", opts = {} },
 	"navarasu/onedark.nvim",
+	{ "folke/neodev.nvim", opts = {} },
 }
