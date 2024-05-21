8fd2c04957725294bd25428c81d5c278ac794d78
Daniel Chico <chico@generalsoftwareinc.com>
2024-05-20 21:19:47 -0400

    feat: add highlight when yanking option

--- lua/theprimeagen/options.lua
+++ lua/theprimeagen/options.lua
@@ -10,3 +10,11 @@ vim.opt.shiftwidth = 4
 vim.opt.showmode = false
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
