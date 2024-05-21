58eae6a597247bfbfdf225a743465555ff3a69b1
Daniel Chico <chico@generalsoftwareinc.com>
2024-05-20 20:58:34 -0400

    fix: remove unnecessary pylint setup option

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,11 +5,7 @@ return {
         null_ls.setup({
             sources = {
                 null_ls.builtins.formatting.stylua,
-                null_ls.builtins.diagnostics.pylint.with({
-                    diagnostics_postprocess = function(diagnostic)
-                        diagnostic.code = diagnostic.message_id
-                    end,
-                }),
+                null_ls.builtins.diagnostics.pylint.with({}),
                 null_ls.builtins.formatting.isort,
                 null_ls.builtins.formatting.black,
                 null_ls.builtins.diagnostics.mypy,
