14b3fa78f794739b28c338826f918425441e32cd
Daniel Chico <chico@generalsoftwareinc.com>
2024-05-20 21:55:09 -0400

    feat: some new keybinds for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,9 +8,19 @@ return {
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>pf", builtin.git_files)
 			vim.keymap.set("n", "<leader>sw", builtin.grep_string)
+			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
 			vim.keymap.set("n", "<leader>ps", function()
 				builtin.grep_string({ search = vim.fn.input("Grep > ") })
 			end)
+			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "[S]earch [H]elp" })
+			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+			vim.keymap.set("n", "<leader>/", function()
+				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
+				builtin.current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+					winblend = 10,
+					previewer = false,
+				}))
+			end, { desc = "[/] Fuzzily search in current buffer" })
 		end,
 	},
 	{
